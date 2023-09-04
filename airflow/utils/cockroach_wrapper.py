bf114c412546a445d2b3945d4934e2318a17d2e1
Scott <hscottvo@gmail.com>
2023-09-04 16:15:23 -0700

    Add cockroach-related dependencies

---
+++ airflow/utils/cockroach_wrapper.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
+import pandas as pd
+
+
+load_dotenv(".env")
+
+
+def cockroach_connection() -> Engine:
+    engine = create_engine(os.environ["COCKROACH_ALCHEMY"])
+    print(type(engine))
+    return engine  # type: ignore
+
+
+def df_to_sql(engine: Engine, df: pd.DataFrame, table: str) -> None:
+    df.to_sql(table, engine, if_exists="replace", index=False)
