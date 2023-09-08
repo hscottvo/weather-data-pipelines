e8e7c6c795dd7e7aa30e5f21c3cf6a3a29a3c0a9
Scott <hscottvo@gmail.com>
2023-09-07 22:26:05 -0700

    Test simple query

--- airflow/streamlit/streamlit_app.py
+++ airflow/streamlit/streamlit_app.py
@@ -1,1 +1,20 @@
+import streamlit as st
 
+import psycopg2 
+
+
+@st.cache_resource
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
+
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("select * from hourly_weather")
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
