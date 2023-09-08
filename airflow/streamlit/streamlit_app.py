10c3090bfec3de7af596b966a43602b8c19afb58
Scott <hscottvo@gmail.com>
2023-09-07 23:15:53 -0700

    Try fixing sql query

--- airflow/streamlit/streamlit_app.py
+++ airflow/streamlit/streamlit_app.py
@@ -1,13 +1,15 @@
 import streamlit as st
 
-import psycopg2 
+import psycopg2
 
 
 @st.cache_resource
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
+
 conn = init_connection()
+
 
 @st.cache_data(ttl=600)
 def run_query(query):
@@ -15,6 +17,7 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("select * from hourly_weather")
+
+rows = run_query("select * from hourly_weather;")
 for row in rows:
     st.write(f"{row[0]} has a :{row[1]}:")
